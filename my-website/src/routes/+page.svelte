8de523d325d3e4571157c9533ebc0ae6341183e3
Penis Joe <hajimemashitekaczynski@gmail.com>
2024-10-17 10:37:18 +0200

    installation

--- my-website/src/routes/+page.svelte
+++ my-website/src/routes/+page.svelte
@@ -1,15 +1,52 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+<!--Delar upp sidan i två delar-->
+<main>
+	<div>
+  
+	  <h1> <b>George Flowberry</b> </h1>
+	  <h2>Since my time in Tilted Towres.. I have figured out how to successfully fabricate a 20 dollar vbuck card...!</h2>
+	  <h3>This is how i Did it:</h3>
+	  <!--Under denna kommer vi lägga till fler element-->
+  
+<p id="inspo">
+    "We need to build a reinforced metal wall." - <strong>Donald Pump</strong>, The 45Th Victory Royale winner in Tilted Towers.
+</p>
+
+<p style="background-color:pink">
+    "I Actually came here just to pick up a Slurpfish." - <span>EatDatSlurpfish445, Pedophile.</span>.
+</p>
+.
+.
+.
+<!-- Lägg till detta inom <style>-taggen -->
+#inspo{
+    font-family: courier;
+    font-size: 160%;
+}
+
+span{
+    color: green;
+}
+
 	</div>
-</div>
+	  <!--Här kommer vi lägga till en bild senare i uppgiften 1.2.4 Extra styling-->  
+  
+  </main>
+  
+  <style>
+	main{
+	  /*Styling för att dela upp sidan i två delar. */
+	  /*Vi kommer gå igenom detta i detalj senare*/
+	  margin: auto;
+	  width: 80vw;
+	  height: 70vh;
+	  display: grid;
+	  grid-template-columns: repeat(2,50%);
+	  overflow: hidden;
+	}
+	div{
+	  /*Styling för att centrera texten i behållaren*/
+	  margin: auto;
+	}
+	</style>
+  
